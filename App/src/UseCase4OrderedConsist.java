bfba6910db554c627dfcc11ebad4632cf20da0e7
Amirtha41206 <amirthaamirtha1238884@gmail.com>
2026-04-07 16:31:20 +0530

    Amirtha Added UC4 Ordered Train Consist using LinkedList

---
+++ App/src/UseCase4OrderedConsist.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class UseCase4OrderedConsist {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        LinkedList<String> trainConsist = new LinkedList<>();
+
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
+
+        trainConsist.add(2, "Pantry Car");
+
+        System.out.println("After adding Pantry Car: " + trainConsist);
+
+        trainConsist.removeFirst();
+        trainConsist.removeLast();
+
+        System.out.println("Final Train Consist: " + trainConsist);
+
+        System.out.println("Program continues...");
+    }
+}
