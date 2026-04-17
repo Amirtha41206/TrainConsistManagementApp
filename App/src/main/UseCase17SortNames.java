600a2c7f567f43e8be9a34cf6d7391245d3ba90d
Amirtha41206 <amirthaamirtha1238884@gmail.com>
2026-04-17 21:33:33 +0530

    Amirtha Updated UC17 sorting logic

--- App/src/main/UseCase17SortNames.java
+++ App/src/main/UseCase17SortNames.java
@@ -2,16 +2,22 @@ package main;
 
 import java.util.Arrays;
 
-public class UseCase17SortNames {
+public class UseCase17SortNames{
 
-    public String[] sortBogieNames(String[] names) {
+    public String[] sortBogieNames(String[] bogies) {
 
-        Arrays.sort(names);
-        return names;
+        Arrays.sort(bogies);
+        return bogies;
     }
 
     public static void main(String[] args) {
 
         String[] bogies = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
 
-        UseCase17SortNames app = new
+        UseCase17TrainConsistMgmt app = new UseCase17TrainConsistMgmt();
+
+        String[] sorted = app.sortBogieNames(bogies);
+
+        System.out.println("Sorted Bogie Names: " + Arrays.toString(sorted));
+    }
+}
