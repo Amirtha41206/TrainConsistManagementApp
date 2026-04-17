a26ea5ed638a91cdac68e53924d27bd072b11f73
Amirtha41206 <amirthaamirtha1238884@gmail.com>
2026-04-17 21:11:09 +0530

    Amirtha Added UC17 Sorting Bogie Names using Arrays.sort()

---
+++ App/src/main/UseCase17TrainConsistMgmt.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.util.Arrays;
+
+public class UseCase17TrainConsistMgmt {
+
+    public String[] sortBogieNames(String[] bogies) {
+
+        Arrays.sort(bogies);
+        return bogies;
+    }
+
+    public static void main(String[] args) {
+
+        String[] bogies = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
+
+        UseCase17TrainConsistMgmt app = new UseCase17TrainConsistMgmt();
+
+        String[] sorted = app.sortBogieNames(bogies);
+
+        System.out.println("Sorted Bogie Names: " + Arrays.toString(sorted));
+    }
+}
